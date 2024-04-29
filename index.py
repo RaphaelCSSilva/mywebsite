2f4507c8b7925ea5e0236ba39b4f3e6734fbaccd
RaphaelCSSilva <raphaelcabidelli20@gmail.com>
2024-04-29 19:03:04 +0100

    Update photos

--- index.py
+++ index.py
@@ -9,6 +9,13 @@ app = Flask(__name__, static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
+# Endpoint for the API '/api/callhome', receives a POST request with a JSON body and save it to the database
+# @app.route('/api/callhome', methods=['POST'])
+# def callhome():
+#     if request.method == 'POST':
+#         print(request.json)
+#         return "OK"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
